5880bfd48989643baaefdec73006df98bdbc43ca
carly <carly@noukod.com>
2018-09-03 18:11:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
 
   board.each do |choix|
-    counter_taken += choix == "X" || choix == "O"? 1 : 0
+    counter += choix == "X" || choix == "O"? 1 : 0
   end
  return counter
 end
