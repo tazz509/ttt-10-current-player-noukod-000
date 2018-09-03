b4ccf5248cfa3164d333244f7626ab0f28ebaf83
carly <carly@noukod.com>
2018-09-03 18:10:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,13 @@
 def turn_count(board)
   counter = 0
-  if board == "X"
-    counter +=1
-    end
+
+  board.each do |choix|
+    counter_taken += choix == "X" || choix == "O"? 1 : 0
+  end
+ return counter
 end
 
-def current_player(board)
 
+def current_player(board)
+  player = turn_count(board)%2 == 0? "X" : "O"
 end
