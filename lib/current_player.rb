e064b6c20bfb8fce12c50e5a879815799e97aa10
carly <carly@noukod.com>
2018-09-03 18:56:43 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |choix|
+    counter += choix == "X" || choix == "O"? 1 : 0
+  end
+ return counter
+end
+
+def current_player(board)
+  player = turn_count(board)%2 == 0? "X" : "O"
+end
